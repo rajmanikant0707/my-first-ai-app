463a67e5dd8cd76649b60485afb6fa695c1ae69f
rajmanikant0707 <rajmanikant5@gmail.com>
2026-05-07 20:43:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,12 @@ from google import genai
 
 st.title("My First AI App 🤖")
 user_input = st.text_input("Ask me anything:")
-
 if st.button("Generate Answer"):
-    # We use a try block to catch the secret and the error safely
-    try:
-        # Step A: Explicitly grab the key
-        my_key = st.secrets["GEMINI_API_KEY"]
-        client = genai.Client(api_key=my_key)
-        
-        with st.spinner("Thinking..."):
-            # Step B: Call the AI
-           response = client.models.generate_content(
-                model='gemini-2.5-flash-lite', 
+    if user_input:
+        with st.spinner("The AI is thinking..."):
+            # Notice how these three lines below are perfectly aligned on the left
+            response = client.models.generate_content(
+                model='gemini-2.5-flash-lite',
                 contents=user_input,
             )
             st.success("Done!")
